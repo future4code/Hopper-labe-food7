c1955d68844c15cebb2283554e1a9145c883adbe
Gustavogaf <gustavoaferreiragt2010@gmail.com>
2022-07-29 01:11:09 -0300

    responsividade do input de cpf e input de confirmação de senha

--- rappi4c/src/Components/Inputs/InputSenha.js
+++ rappi4c/src/Components/Inputs/InputSenha.js
@@ -1,14 +1,14 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import IconButton from '@mui/material/IconButton';
-import Input from '@mui/material/Input';
-import FilledInput from '@mui/material/FilledInput';
+// import Input from '@mui/material/Input';
+// import FilledInput from '@mui/material/FilledInput';
 import OutlinedInput from '@mui/material/OutlinedInput';
 import InputLabel from '@mui/material/InputLabel';
 import InputAdornment from '@mui/material/InputAdornment';
-import FormHelperText from '@mui/material/FormHelperText';
+// import FormHelperText from '@mui/material/FormHelperText';
 import FormControl from '@mui/material/FormControl';
-import TextField from '@mui/material/TextField';
+// import TextField from '@mui/material/TextField';
 import Visibility from '@mui/icons-material/Visibility';
 import VisibilityOff from '@mui/icons-material/VisibilityOff';
 
@@ -40,7 +40,7 @@ export default function InputSenha(props) {
     <Box sx={{ display: 'flex', flexWrap: 'wrap' }}>
       <div>
         <FormControl sx={{m:1, width: '41ch'}} variant="outlined">
-          <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+          <InputLabel htmlFor="outlined-adornment-password">Senha</InputLabel>
           <OutlinedInput
             id="outlined-adornment-password"
             type={values.showPassword ? 'text' : 'password'}
